e549cb2ae3ed409097a5e682a0321cb8d72c89eb
efeca <efecaliskan08@gmail.com>
2025-06-15 17:13:14 +0300

    refactor worker function, enhance error handling and fix threading safety

--- SpiBerryEngine.py
+++ SpiBerryEngine.py
@@ -3,10 +3,13 @@ from time import sleep
 
 import RPi.GPIO as GPIO # type: ignore
 from RGBLED import RGBLED
-import sys
 
+import keyboard
+import sys
+import serial.serialutil
+import importlib
 try:
-    from raspi_functions import *
+    import raspi_functions
 except ImportError:
     pass
 
@@ -38,10 +41,38 @@ def stop(state:State):
 
 with open("robot_code.py","r") as f:
     code = f.read()
+    
+work_event = threading.Event()
+worker_thread = None
 
-work = False
+def read_function_call(state:State):
+    try:
+        func_call = state.transport.read_until(8, b":",timeout=999)
+    except serial.serialutil.SerialException as e:
+        if "ClearCommError" in str(e):
+            if not work_event.is_set():
+                sys.exit(1)
+            print("Serial port error")
+        else:
+            raise e
+    func_call = func_call.decode()[:-1].strip()
+    return func_call
+
+def run_function(func_call:str):
+    result_string = ""
+    if not func_call=="" and func_call.isprintable():
+        try:
+            result_string = eval("raspi_functions."+func_call)
+        except NameError as e:
+            rgbLED.blink("blue", duration=0.5, count=2)
+            print("NameError in function call: ", e)
+            result_string = ""
+        except Exception as e:
+            print("Error in function call: ", e)
+            result_string = ""
+    return result_string
+
 def worker():
-    global work
     try:
         state.transport.enter_raw_repl()
     except transport.TransportError as e:
@@ -49,54 +80,60 @@ def worker():
         print("Error entering raw REPL:", e)
         sys.exit(0)
     try:
-        state.transport.exec_raw_no_follow(code)    
+        state.transport.exec_raw_no_follow(code)
     except transport.TransportError as e:
         rgbLED.blink("red", duration=0.2, count=3)
         print("Error executing code:", e)
         sys.exit(0)
+
     rgbLED.green()
-    func_call = state.transport.read_until(8,b":")
-    func_call =func_call.decode()[:-1].strip()
-    print("func call:",func_call)
     
-    while func_call!="***" and work:
+    func_call = read_function_call(state)
+    
+    while func_call!="exit" and work_event.is_set():
         sleep(0.05)
         print("Function call: ",func_call)
-        result_string = ""
-        if not func_call=="" and func_call.isprintable():
-            try:
-                result_string = eval(func_call)
-            except NameError as e:
-                rgbLED.blink("blue", duration=0.5, count=2)
-                print("NameError in function call: ", e)
-                result_string = ""
-            except Exception as e:
-                print("Error in function call: ", e)
-                result_string = ""
+        
+        result_string = run_function(func_call)
+        
         state.transport.serial.write(f"{result_string}\r\n".encode())
+        state.transport.read_until(8,result_string.encode()) # clean up the acknowledgment
         
-        # state.transport.follow(timeout=10,data_consumer=lambda x: print("Follow Data: ",x,"\n\n"))
-        
-        func_call = state.transport.read_until(8,b":")
-        func_call = func_call.decode()[:-1].strip()    
-
+        func_call = read_function_call(state)
+    print("Code finished.")
+    work_event.clear()
+    
 try:
     while True:
         if GPIO.input(11)==0:
-            if work:
+        # if keyboard.is_pressed('q'):
+            if work_event.is_set():
                 print("Button pressed, stopping work...")
-                work = False
+                work_event.clear()
                 stop(state)
                 rgbLED.red()
             else:
-                t = threading.Thread(target=worker)
-                t.start()
+                with open("robot_code.py","r") as f:
+                    code = f.read()
+                importlib.reload(raspi_functions)
+                work_event.set()
+                worker_thread = threading.Thread(target=worker)
+                worker_thread.start()
                 print("Button pressed, starting work...")
-                work = True
+
                 rgbLED.yellow()
+
             sleep(0.3)
 except Exception as e:
-    print("error occurred: ",e)
+    print("Error occurred: ",e)
+except KeyboardInterrupt:
+    print("KeyboardInterrupt received, stopping...")
+    if work_event.is_set():
+        work_event.clear()
+        stop(state)
+        rgbLED.red()
+    else:
+        print("No work to stop.")
 finally:
     print("Cleaning up GPIO")
     GPIO.cleanup()
